7f573e5b2bee0d161f339e570769ad96c8b35873
Jordan Truong <jordandt123411@gmail.com>
2024-11-14 17:27:53 -0800

    Initial Commit (Server): Setup the basics of the backend and connected to MongoDB database.

---
+++ server/src/models/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connect = mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.htm2g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
+
+// Check if database connected
+connect
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((error) => {
+    console.log("Database cannot be connected.", error);
+  });
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
+});
+
+// Collection part
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
---
+++ server/src/server.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const http = require("http");
+const rateLimiter = require("express-rate-limit");
+const slowDown = require("express-slow-down");
+const connect = require("./models/db");
+const app = express();
+
+// Middleware
+const { NODE_ENV, WEB_URL } = process.env;
+const notProduction = NODE_ENV !== "production";
+app.use(helmet());
+app.use(
+  cors({
+    origin: notProduction ? "http://localhost:3000" : WEB_URL,
+  })
+);
+app.use(express.json());
+if (notProduction) {
+  app.use(morgan("dev"));
+} else {
+  app.enable("trust proxy");
+  app.set("trust proxy", 1);
+}
+
+// Rate & Speed Limiter Info
+const timeLimit = 1000 * 60 * 15;
+const maxReq = 75;
+const limiter = rateLimiter({
+  windowMs: timeLimit,
+  max: maxReq,
+});
+const speedLimiter = slowDown({
+  windowMs: timeLimit,
+  delayAfter: maxReq / 2,
+  delayMs: () => 500,
+});
+
+// Rate & Speed Limiters
+app.use(speedLimiter);
+app.use(limiter);
+
+// Mongoose Connection
+connect();
+
+// Routes
+const API_VERSION = `v${process.env.API_VERSION}`;
+
+// Landing Page Route
+app.get("/", (req, res) => {
+  res.send("Spartan Finance API is Up and Running !");
+});
+
+// API Routes
+
+// Users
+// app.use(`/${API_VERSION}/api/users`, require("./routes/user.rt"));
+
+// Bank
+// app.use(`/${API_VERSION}/api/bank/:id`, authUser, require("./routes/bank.rt"));
+
+// PORT and Sever
+const PORT = process.env.PORT || 6000;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`CORS Enabled Server, Listening to port: ${PORT}...`);
+});
+
+// Export the Express API
+module.exports = server;
